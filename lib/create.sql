8a25c1e8601e08202dd57af9e2f793f2f131f64b
Gideon <keterjr1@gmail.com>
2024-06-06 14:40:02 +0300

    pass tests

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  sex TEXT,
+  age INTEGER,
+  color TEXT,
+  temperament TEXT,
+  alive INTEGER
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, sex, age, color, temperament, alive) VALUES ('Mr. Chocolate', 'M', 34, 'White', 'Mkali', 0);
+INSERT INTO bears(name, sex, age, color, temperament, alive) VALUES ('Rowdy', 'M', 24, 'White', 'Mkali', 0);
+INSERT INTO bears(name, sex, age, color, temperament, alive) VALUES ('Tabitha', 'F', 54, 'Black', 'Mpole', 0);
+INSERT INTO bears(name, sex, age, color, temperament, alive) VALUES ('Sergeant Brown', 'M', 45, 'White', 'Mkali', 0);
+INSERT INTO bears(name, sex, age, color, temperament, alive) VALUES ('Melissa', 'F', 20, 'White', 'Mpole', 0);
+INSERT INTO bears(name, sex, age, color, temperament, alive) VALUES ('Grinch', 'M', 19, 'White', 'Mfiti', 0);
+INSERT INTO bears(name, sex, age, color, temperament, alive) VALUES ('Wendy', 'F', 29, 'White', 'Mlukewarm', 0);
+INSERT INTO bears(name, sex, age, color, temperament, alive) VALUES (NULL, 'F', 29, 'White', 'Mlukewarm', 0);
